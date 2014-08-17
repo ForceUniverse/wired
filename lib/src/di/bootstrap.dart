8f457c246c653f63df67fa55ea4a3c3e147a462e
luis <luisbigvargas@gmail.com>
2014-08-17 18:32:34 -0400

    Add to src2
    Conflicts:

--- lib/src/di/bootstrap.dart
+++ lib/src/di/bootstrap.dart
@@ -1,54 +1,63 @@
 part of force_it;
-
+/**
+ * Class that is in charge of mantaining the Application Context.
+ * ie. list of Components, Controllers, ...
+ */
 class ApplicationContext {
   static Map<String, Object> _singletons = new Map<String, Object>();
-  static List<Object> components = new List<Object>();
+  static List<Object> components = [];
   static MessagesContext _messageContext = new MessagesContext();
   
   static void registerMessage(String key, String yamlContent) {
     _messageContext.register(key, yamlContent);
   }
-  
+
+  /**
+   * Bootstrap the Application Context. ie. Scan for components
+   * annotated with @Config or @Component and inject the @Values
+   * and @Autowired
+   */
   static void bootstrap() {
     // first bootstrap your beans
-    Scanner<_Config, Object> classesHelper = new Scanner<_Config, Object>();
-    List<Object> classes = classesHelper.scan();
-          
-    for (var obj in classes) {
-        _register(_injectValue(obj));
-    }
-    
-    // search for Components and then inject 
-    Scanner<Component, Object> componentHelper = new Scanner<Component, Object>();
-    
-    // inject the autowired
+    new Scanner<_Config>().scan().forEach((obj) =>
+        _register(_injectValue(obj))
+    );
+
+    // inject the _Autowired
     _singletons.forEach((key, value) {
       _inject(_injectValue(value));
     });
+
+    // search for Components and then inject
+    addComponents(new Scanner<Component>().scan());
+  }
+
+  /**
+   * Add the list of components and inject them the @Value and @Autowired
+   */
+  static Iterable addComponents(Iterable scannedComponents) {
+    for (var scannedComponent in scannedComponents) {
+        // do something with the component
+        components.add(_injectValue(scannedComponent));  
+    }
     
-    component(componentHelper);
+    for (var scannedComponent in components) {
+        // do something with the components
+        _inject(scannedComponent);  
+    }
+    return scannedComponents;
   }
-  
-  static List component(Scanner scanner) {
-        List componentsScanned = scanner.scan();
-                
-        for (var component in componentsScanned) {
-            // do something with the component
-            components.add(_injectValue(component));  
-        }
-        
-        for (var component in components) {
-            // do something with the components
-            _inject(component);  
-        }
-        return componentsScanned;
-  }
-  
+
+  /**
+   * Gets the Named Bean declared in @Config files
+   */
   static Object getBean(String name) => _singletons[name];
-  
+
+  /**
+   * Inject the variables annotated with @[Autowired]
+   */
   static Object _inject(Object obj) {
-    MetaDataHelper<_Autowired> varMirrorHelper = new MetaDataHelper<_Autowired>();
-    List<MetaDataValue<_Autowired>> varMirrorModels = varMirrorHelper.getVariableMirrorValues(obj);
+    List<MetaDataValue<_Autowired>> varMirrorModels = new MetaDataHelper<_Autowired, VariableMirror>().from(obj);
           
     for(MetaDataValue<_Autowired> varMM in varMirrorModels) {
        var value = _singletons[varMM.name];
@@ -58,10 +67,12 @@ class ApplicationContext {
     }
     return obj;
   }
-  
+
+  /**
+   * inject the value of the @[Value] annotation
+   */
   static Object _injectValue(Object obj) {
-    MetaDataHelper<Value> varMirrorHelper = new MetaDataHelper<Value>();
-    List<MetaDataValue<Value>> varMirrorModels = varMirrorHelper.getVariableMirrorValues(obj);
+    List<MetaDataValue<Value>> varMirrorModels = new MetaDataHelper<Value, VariableMirror>().from(obj);
     
     for(MetaDataValue<Value> varMM in varMirrorModels) {
        Value value = varMM.object;
@@ -69,13 +80,19 @@ class ApplicationContext {
     }
     return obj;
   }
-  
+
+  /**
+   * Gets the value of the annotation @[Value]
+   */
   static String getValue(String key, {String defaultValue: ""}) {
     return _messageContext.message(key, defaultValue: defaultValue);
   }
-  
+
+  /**
+   * Register all elements annotated with @Bean
+   */
   static void _register(Object obj) {
-       List<MetaDataValue<_Bean>> mirrorValues = new MetaDataHelper<_Bean>().getMirrorValues(obj);
+       List<MetaDataValue<_Bean>> mirrorValues = new MetaDataHelper<_Bean, MethodMirror>().from(obj);
             
        for (MetaDataValue mv in mirrorValues) {
             InstanceMirror res = mv.invoke([]);
